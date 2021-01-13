77c3a7feb2e6a35f4604659a3bc8affc9a78a48b
MJ-Jang <xkxpa@korea.ac.kr>
2021-01-13 13:50:57 +0000

    ud

--- data/get_dataset.py
+++ data/get_dataset.py
@@ -189,11 +189,10 @@ def get_attention_keyword(dataset, attn_model, keyword_per_class=10):
         tokens = tokens.to(device)
 
         with torch.no_grad():
-            print(attn_model(tokens).size())
             attention_layers = attn_model(tokens)
+            print(attention_layers.keys())
 
-        attention = attention_layers[0]  # attention of final layer (batch_size, num_heads, max_len, max_len) # 0 for distil bert
-        print(attention.size())
+        attention = attention_layers[-1]  # attention of final layer (batch_size, num_heads, max_len, max_len) # 0 for distil bert
         attention = attention.sum(dim=1)  # sum over attention heads (batch_size, max_len, max_len)
 
         for i in range(attention.size(0)):  # batch_size
